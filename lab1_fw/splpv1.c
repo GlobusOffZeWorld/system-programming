480614493ac06bfc0942cc2dc1b40fe5b598ef34
GlobusOffZeWorld <gleb737.gl@gmail.com>
2021-10-17 21:11:10 +0300

    make lab

--- lab1_fw/splpv1.c
+++ lab1_fw/splpv1.c
@@ -4,11 +4,10 @@
  * This file contains validation of SPLPv1 protocol. 
  */
 
-
-#error Specify your name and group
 /*
   Фамилия, Имя, Отчество
-  № группы
+  Полывяный. Глеб. Андреевич
+  № группы - 13
 */
 
 
@@ -62,30 +61,155 @@ IN CASE OF INVALID MESSAGE THE STATE SHOULD BE RESET TO 1 (INIT)
 
 
 #include "splpv1.h"
-
-
-
-
+#include <string.h>
 
 /* FUNCTION:  validate_message
- * 
- * PURPOSE:  
- *    This function is called for each SPLPv1 message between client 
+ *
+ * PURPOSE:
+ *    This function is called for each SPLPv1 message between client
  *    and server
- * 
+ *
  * PARAMETERS:
- *    msg - pointer to a structure which stores information about 
+ *    msg - pointer to a structure which stores information about
  *    message
- * 
+ *
  * RETURN VALUE:
- *    MESSAGE_VALID if the message is correct 
- *    MESSAGE_INVALID if the message is incorrect or out of protocol 
+ *    MESSAGE_VALID if the message is correct
+ *    MESSAGE_INVALID if the message is incorrect or out of protocol
  *    state
  */
-enum test_status validate_message( struct Message *msg )
-{
 
-    // TODO: Implement me
+const char base64[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+                       0, 0, 0, 1, 1, 1,
+                       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+                       1, 1, 1, 1, 1, 1,
+                       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0};
 
-    return MESSAGE_VALID;     
+const char data[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+                     1, 1, 1, 1, 1,
+                     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0};
+
+int state = 1;
+const char *commands[] = {"GET_DATA", "GET_FILE", "GET_COMMAND"};
+int commandIndex;
+
+enum test_status validate_message(struct Message *msg) {
+  char *message = msg->text_message;
+  if (msg->direction == 0) {
+    if (state == 1) {
+      if (strcmp(message, "CONNECT") == 0) {
+        state = 2;
+        return MESSAGE_VALID;
+      }
+    } else if (state == 3) {
+      if (strcmp(message, "GET_VER") == 0) {
+        state = 4;
+        return MESSAGE_VALID;
+      } else if (strcmp(message, "GET_DATA") == 0) {
+        commandIndex = 0;
+        state = 5;
+        return MESSAGE_VALID;
+      } else if (strcmp(message, "GET_FILE") == 0) {
+        commandIndex = 1;
+        state = 5;
+        return MESSAGE_VALID;
+      } else if (strcmp(message, "GET_COMMAND") == 0) {
+        commandIndex = 2;
+        state = 5;
+        return MESSAGE_VALID;
+      } else if (strcmp(message, "GET_B64") == 0) {
+        state = 6;
+        return MESSAGE_VALID;
+      } else if (strcmp(message, "DISCONNECT") == 0) {
+        state = 7;
+        return MESSAGE_VALID;
+      }
+    }
+  } else if (msg->direction == 1) {
+    if (state == 2) {
+      if (strcmp(message, "CONNECT_OK") == 0) {
+        state = 3;
+        return MESSAGE_VALID;
+      }
+    } else if (state == 4) {
+      if (strncmp(message, "VERSION ", 8) == 0) {
+        message += 8;
+        if (*message > 48 && *message < 58) {
+          for (++message; *message != '\0'; ++message) {
+            if (*message < 48 || *message > 57) {
+              state = 1;
+              return MESSAGE_INVALID;
+            }
+          }
+          state = 3;
+          return MESSAGE_VALID;
+        }
+      }
+    } else if (state == 5) {
+      if (strncmp(message, commands[commandIndex], strlen(commands[commandIndex])) == 0) {
+        message += strlen(commands[commandIndex]);
+        if (*message == ' ') {
+          ++message;
+          char *spaceCheck;
+          while (data[*message]) {
+            ++message;
+          }
+          spaceCheck = (char *) (*message == ' ');
+          if (spaceCheck) {
+            spaceCheck = message + 1;
+          }
+
+          if (spaceCheck) {
+            if (strcmp(spaceCheck, commands[commandIndex]) == 0) {
+              state = 3;
+              return MESSAGE_VALID;
+            }
+          }
+          ++message;
+        }
+      }
+    } else if (state == 6 && (strncmp(message, "B64: ", 5) == 0)) {
+      message += 5;
+      char *begin = message;
+      while (base64[*message]) {
+        ++message;
+      }
+      char check = 0;
+      while ((check < 2) && (message[check] == '=')) {
+        ++check;
+      }
+      if ((message - begin + check) % 4 == 0 && message[check] == 0) {
+        state = 3;
+        return MESSAGE_VALID;
+      }
+      ++message;
+    } else if (state == 7) {
+      if (strcmp(message, "DISCONNECT_OK") == 0) {
+        state = 1;
+        return MESSAGE_VALID;
+      }
+    }
+  }
+
+  state = 1;
+
+  return MESSAGE_INVALID;
 }
