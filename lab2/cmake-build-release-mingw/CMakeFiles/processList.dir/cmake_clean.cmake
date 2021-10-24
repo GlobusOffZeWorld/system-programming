3d1b3a218cb75f5d56e4af908fcd60d1eebc4419
GlobusOffZeWorld <gleb737.gl@gmail.com>
2021-10-24 21:30:33 +0300

    make task

---
+++ lab2/cmake-build-release-mingw/CMakeFiles/processList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/processList.dir/main.c.obj"
+  "libprocessList.dll.a"
+  "processList.exe"
+  "processList.exe.manifest"
+  "processList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/processList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab2/cmake-build-release-visual-studio/CMakeFiles/processList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/processList.dir/main.c.obj"
+  "processList.exe"
+  "processList.exe.manifest"
+  "processList.lib"
+  "processList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/processList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
